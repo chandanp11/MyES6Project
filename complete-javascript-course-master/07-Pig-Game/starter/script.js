1d7a13a29bdbce276965ecf913a94cf9b21b525c
Chandan Patil <chandan.p11@gmail.com>
2022-07-05 09:32:58 +0530

    added roll dice pig game

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,94 @@
 'use strict';
+
+// Selecting Elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const currentScore0Element = document.getElementById('current--0');
+const currentScore1Element = document.getElementById('current--1');
+
+const diceElement = document.querySelector('.dice');
+const buttonNewGame = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, isGameActive;
+
+const initializeGame = function () {
+  scores = [0, 0];
+
+  currentScore = 0;
+  activePlayer = 0;
+  isGameActive = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  currentScore0Element.textContent = 0;
+  currentScore1Element.textContent = 0;
+
+  diceElement.classList.add('hidden');
+  player0Element.classList.remove('player--winner');
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.add('player--active');
+  player1Element.classList.remove('player--active');
+};
+
+initializeGame();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+buttonRoll.addEventListener('click', function () {
+  if (isGameActive) {
+    // generating random dice variable between 1 and 6
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // display dice
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
+
+    // check for rolled 1. If yes, switch to next player
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // currentScore0Element.textContent = currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+buttonHold.addEventListener('click', function () {
+  if (isGameActive) {
+    // add current score to active player score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // check if current player score >= 100, then finish the game
+    if (scores[activePlayer] >= 20) {
+      isGameActive = false;
+      diceElement.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+buttonNewGame.addEventListener('click', initializeGame);
